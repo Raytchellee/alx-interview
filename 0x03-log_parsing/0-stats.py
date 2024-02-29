f532792922b3d170204943d40c2fa143b5cadf5c
raytchellee <amakaoforkaja@gmail.com>
2024-02-29 18:06:56 +0000

    check

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,26 +4,26 @@
 
 import sys
 
-status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
-                      '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-line_counter = 0
+statusCodeCounts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                    '403': 0, '404': 0, '405': 0, '500': 0}
+totalSize = 0
+lineCounter = 0
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
-        if len(line_list) > 4:
-            code = line_list[-2]
-            size = int(line_list[-1])
-            if code in status_code_counts.keys():
-                status_code_counts[code] += 1
-            total_size += size
-            line_counter += 1
+        lineList = line.split(" ")
+        if len(lineList) > 4:
+            code = lineList[-2]
+            size = int(lineList[-1])
+            if code in statusCodeCounts.keys():
+                statusCodeCounts[code] += 1
+            totalSize += size
+            lineCounter += 1
 
-        if line_counter == 10:
-            line_counter = 0
-            print('File size: {}'.format(total_size))
-            for key, value in sorted(status_code_counts.items()):
+        if lineCounter == 10:
+            lineCounter = 0
+            print('File size: {}'.format(totalSize))
+            for key, value in sorted(statusCodeCounts.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
@@ -31,8 +31,7 @@ except Exception as err:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
-    for key, value in sorted(status_code_counts.items()):
+    print('File size: {}'.format(totalSize))
+    for key, value in sorted(statusCodeCounts.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
-
