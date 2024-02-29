98ae545caf8eb9266f673aa1c602a450373436e0
raytchellee <amakaoforkaja@gmail.com>
2024-02-29 18:04:10 +0000

    checking

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,42 +3,36 @@
 
 
 import sys
-import signal
 
-
-def signal_handler(sig, frame):
-    print_stats()
-    sys.exit(0)
-
-
-def print_stats():
-    total_size = sum(file_sizes.values())
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-
-
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-file_sizes = {}
-
-line_count = 0
-
-signal.signal(signal.SIGINT, signal_handler)
+status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+line_counter = 0
 
 try:
     for line in sys.stdin:
-        parts = line.split()
-        if len(parts) >= 7:
-            ip, _, _, date, _, status, size = parts[:7]
-            if status.isdigit() and int(status) in status_codes:
-                status_code = int(status)
-                file_size = int(size)
-                file_sizes[line_count] = file_size
-                status_codes[status_code] += 1
-                line_count += 1
-                if line_count % 10 == 0:
-                    print_stats()
-except KeyboardInterrupt:
-    print_stats()
-    sys.exit(0)
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in status_code_counts.keys():
+                status_code_counts[code] += 1
+            total_size += size
+            line_counter += 1
+
+        if line_counter == 10:
+            line_counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(status_code_counts.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_code_counts.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
+
