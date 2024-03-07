f25c074c2e2bb3e028f275ad8b60b340ba1703dd
raytchellee <amakaoforkaja@gmail.com>
2024-03-07 17:26:50 +0000

    task check

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,32 +1,30 @@
 #!/usr/bin/python3
 """UTF-8 Validation"""
 
+
 def validUTF8(data):
-    def is_start_byte(byte):
-        """UTF-8 Validation"""
-        return (byte >> 7) == 0 or \
-               (byte >> 5) == 0b110 or \
-               (byte >> 4) == 0b1110 or \
-               (byte >> 3) == 0b11110
-
-    def is_continuation_byte(byte):
-        return (byte >> 6) == 0b10
-
-    remaining_bytes = 0
+    """
+    Determines if a given data set represents a valid UTF-8 encoding.
+    Args:
+        data (list): A list of integers representing bytes of data.
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else False.
+    """
+    bytes_to_check = 0
 
     for byte in data:
-        if remaining_bytes > 0:
-            if not is_continuation_byte(byte):
-                return False
-            remaining_bytes -= 1
-        else:
-            if not is_start_byte(byte):
-                return False
+        if bytes_to_check == 0:
             if (byte >> 5) == 0b110:
-                remaining_bytes = 1
+                bytes_to_check = 1
             elif (byte >> 4) == 0b1110:
-                remaining_bytes = 2
+                bytes_to_check = 2
             elif (byte >> 3) == 0b11110:
-                remaining_bytes = 3
+                bytes_to_check = 3
+            elif (byte >> 7):
+                return False
+        else:
+            if (byte >> 6) != 0b10:
+                return False
+            bytes_to_check -= 1
 
-    return remaining_bytes == 0
+    return bytes_to_check == 0
