44e60141e8b7cd7ad3880988cafcdc71f48ac6ad
raytchellee <amakaoforkaja@gmail.com>
2024-03-07 17:22:58 +0000

    task check

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+def validUTF8(data):
+    def is_start_byte(byte):
+        return (byte >> 7) == 0 or \
+               (byte >> 5) == 0b110 or \
+               (byte >> 4) == 0b1110 or \
+               (byte >> 3) == 0b11110
+
+    def is_continuation_byte(byte):
+        return (byte >> 6) == 0b10
+
+    remaining_bytes = 0
+
+    for byte in data:
+        if remaining_bytes > 0:
+            if not is_continuation_byte(byte):
+                return False
+            remaining_bytes -= 1
+        else:
+            if not is_start_byte(byte):
+                return False
+            if (byte >> 5) == 0b110:
+                remaining_bytes = 1
+            elif (byte >> 4) == 0b1110:
+                remaining_bytes = 2
+            elif (byte >> 3) == 0b11110:
+                remaining_bytes = 3
+
+    return remaining_bytes == 0
---
+++ 0x04-utf8_validation/README.md
@@ -0,0 +1,1 @@
+A directory for utf-8 validation
