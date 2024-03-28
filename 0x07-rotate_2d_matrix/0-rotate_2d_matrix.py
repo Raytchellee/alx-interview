0030de09d21b93ce2791bcac96b84f1c05af06de
raytchellee <amakaoforkaja@gmail.com>
2024-03-28 17:51:53 +0000

    testing

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Rotate 2D Array"""
+
+def rotate_2d_matrix(matrix):
+    """Main roataion function"""
+    n = len(matrix)
+    for i in range(n // 2):
+        for j in range(i, n - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[n - j - 1][i]
+            matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1]
+            matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1]
+            matrix[j][n - i - 1] = temp
