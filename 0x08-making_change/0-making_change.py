a78ae7796e17c643211f708e6e64ae05474e2aa2
Ray Ray <101654980+Raytchellee@users.noreply.github.com>
2024-04-03 19:00:18 +0100

    Update 0-making_change.py

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,12 @@ def makeChange(coins, total):
     if total < 1:
         return 0
 
-    max_val = total + 1
-    dp = [max_val] * (total + 1)
-    dp[0] = 0
+    coins.sort(reverse=True)
+    num_coins = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if coin <= i:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
+    for coin in coins:
+        while total >= coin:
+            total -= coin
+            num_coins += 1
 
-    return dp[total] if dp[total] < max_val else -1
+    return num_coins if total == 0 else -1
