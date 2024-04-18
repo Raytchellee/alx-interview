98484dc263afc82d32c884ca15bcbcc2871bd022
Ray Ray <amakaoforkaja@gmail.com>
2024-04-18 17:02:36 +0100

    check

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -2,41 +2,31 @@
 """Prime game solution"""
 
 
+def getPrimes(idx):
+    """Prime game solution"""
+    numbers = []
+    diff = [True] * (idx + 1)
+    for num in range(2, idx + 1):
+        if diff[num]:
+            numbers.append(num)
+            for idy in range(num, idx + 1, num):
+                diff[idy] = False
+    return numbers
+
+
 def isWinner(x, nums):
     """Prime game solution"""
-    def sieve_of_eratosthenes(limit):
-        """Prime game solution"""
-        primes = [True] * (limit + 1)
-        primes[0] = primes[1] = False
-        p = 2
-        while p * p <= limit:
-            if primes[p]:
-                for i in range(p * p, limit + 1, p):
-                    primes[i] = False
-            p += 1
-        return primes
-
-    def get_winner(primes):
-        """Prime game solution"""
-        prime_count = sum(primes)
-        if prime_count % 2 == 0:
-            return "Ben"
-        return "Maria"
-
-    winners = []
-    max_limit = max(nums)
-    primes = sieve_of_eratosthenes(max_limit)
-    
-    for n in nums:
-        prime_count = sum(primes[:n+1])
-        winners.append(get_winner(primes[:n+1]))
-
-    maria_wins = winners.count("Maria")
-    ben_wins = winners.count("Ben")
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
+    count_m = count_b = 0
+    for idy in range(x):
+        all_primes = getPrimes(nums[idy])
+        if len(all_primes) % 2 == 0:
+            count_b += 1
+        else:
+            count_m += 1
+    if count_m > count_b:
+        return 'Maria'
+    elif count_b > count_m:
+        return 'Ben'
+    return None
